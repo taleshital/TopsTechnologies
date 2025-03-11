9f44025674c39ca6b0dc6c8a43eb84dc6f0146c2
taleshital <taleshital28@gmail.com>
2025-03-11 12:25:33 +0530

    Add files via upload

---
+++ React js/Assingment3/Navigation.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navigation = () => {
+    return (
+        <>
+            <Nav>
+                <link to='/HomePage'>Home</link>
+                <link to='/AboutPage'>About</link>
+                <link to='/Contact'>Contact</link>
+
+            </Nav>
+
+        </>
+    )
+}
+
+export default Navigation
